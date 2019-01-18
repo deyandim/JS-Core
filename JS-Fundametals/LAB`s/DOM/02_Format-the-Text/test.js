68889a4eb21f24dd86592c7bfd501bf0e3dd651f
Deyan Dimov <41387847+deyandim@users.noreply.github.com>
2019-01-18 23:27:15 +0200

    upload

--- JS-Fundametals/LAB`s/DOM/02_Format-the-Text/test.js
+++ JS-Fundametals/LAB`s/DOM/02_Format-the-Text/test.js
@@ -7,7 +7,6 @@ function solve() {
     arr.pop();
 
     for (let el of arr) {
-
         if (count != 3) {
             if (!(key in obj)) {
                 obj[key] = el + ".";
@@ -27,15 +26,8 @@ function solve() {
     }
 
     for (let paraph in obj) {
-        // console.log(obj[paraph]);
         let p1 = document.createElement("p");
         document.getElementById("output").appendChild(p1);
         p1.textContent = obj[paraph];
     }
-    // console.log(obj);
-
-
 }
-
-
-solve("JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables interactive web pages and thus is an essential part of web applications. The vast majority of websites use it, and all major web browsers have a dedicated JavaScript engine to execute it. As a multi-paradigm language, JavaScript supports event-driven, functional, and imperative (including object-oriented and prototype-based) programming styles. It has an API for working with text, arrays, dates, regular expressions, and basic manipulation of the DOM, but the language itself does not include any I/O, such as networking, storage, or graphics facilities, relying for these upon the host environment in which it is embedded.");
