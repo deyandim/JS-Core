f7c2d76f287284d78900e1551de4f61bdd20204b
Deyan Dimov <41387847+deyandim@users.noreply.github.com>
2019-02-09 00:52:02 +0200

    upload

--- JS-Fundametals/JS-FundExamPreparation/03.ThirdProblm-Extractor/solution.js
+++ JS-Fundametals/JS-FundExamPreparation/03.ThirdProblm-Extractor/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-    // TODO
+    let len = '';
+    let lastResult = '';
+    let output = document.getElementById('output');
+
+    let extractBtn = document.getElementsByTagName('button')[0];
+    extractBtn.addEventListener('click', extract);
+
+    function extract() {
+        let input = document.getElementById('input').value;
+        let number = input.match(/[0-9]+/);
+        input = input.replace(number, '');
+        input = input.substring(0, number);
+        input = input.split(`${input[input.length - 1]}`).filter(x => x != 0);
+
+        let pattern = new RegExp(`[${input[0]}]+`, 'g');
+        output.value = input[1].split(pattern).join('').split('#').join(' ');
+    }
 }
